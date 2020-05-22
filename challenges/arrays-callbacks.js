71a464344d64c2da23485877b34f7e48c81e65fa
BuildTools <unconfigured@null.spigotmc.org>
2020-05-22 10:55:34 -0500

    task 4 complete

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -54,10 +54,13 @@ console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. Find the total population from all 
+the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce(function(accu, item){
+  return Number(accu) + Number(item.population)
+},0);
 console.log(populationTotal);
 
 
@@ -69,7 +72,9 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+const consume = function(a, b, cb){
+  return cb(a,b)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
